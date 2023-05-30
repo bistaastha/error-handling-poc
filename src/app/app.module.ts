11bf9775165b41ec1b1c2732a3c5c01f01969459
aastha <aastha.b@fyle.in>
2023-05-30 07:41:17 +0530

    Minor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BaseModule } from './base-module/base-module.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BaseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
